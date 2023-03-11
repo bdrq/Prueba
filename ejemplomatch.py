17916a4c1f3b86af3aeaf3d97c3674765ed84b49
BRAYROQU <brayandavidrodriguezquintero@gmail.com>
2023-03-10 19:45:46 -0500

    Se realiza carga de archivo

---
+++ ejemplomatch.py
@@ -0,0 +1,7 @@
+variable = input("Este es un valor que necesitamos buscar=>")
+if variable == "Persona":
+    print("Bienvenido humano")
+elif variable == "Maquina":
+    print("No se le permite entrar a bots")
+else:
+    print("Que clase de criatura eres tu?")
